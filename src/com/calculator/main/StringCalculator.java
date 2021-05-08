98a7c7b0ca8e2ed20501244d98f05978826d3074
Ritu2503 <kritu2503@gmail.com>
2021-05-09 00:01:13 +0530

    Adding delimiters testcase

--- src/com/calculator/main/StringCalculator.java
+++ src/com/calculator/main/StringCalculator.java
@@ -4,15 +4,28 @@ public class StringCalculator {
 	
 	public int add(String numbers)
 	{
-		
+	int sum=0;	
 		if(numbers=="")
 		{
 			return 0;
 		}
 		
+		char[]ch= new char[numbers.length()];
+		for(int i=0;i<numbers.length();i++)
+		{
+		ch[i]=numbers.charAt(i);
+		}
+		 for (char c : ch)
+	{
+		if (Character.isDigit(c))
+		{
+			int a= Integer.parseInt(String.valueOf(c));
+		sum= sum + a;
 		
-		return 0;
-		
+		}
 	}
+		return sum;		
+	}
+	
 
 }
--- src/com/calculator/main/StringCalculatorTest.java
+++ src/com/calculator/main/StringCalculatorTest.java
@@ -9,10 +9,20 @@ public class StringCalculatorTest {
 	StringCalculator stringcalc = new StringCalculator();
 
 	@Test
-	public void test1(){
-		
+	public void test1(){		
 		assertEquals(0, stringcalc.add(""));
-		
+		assertEquals(1, stringcalc.add("1"));
+		assertEquals(9, stringcalc.add("9"));
+		assertEquals(3, stringcalc.add("1,2"));
+		assertEquals(11, stringcalc.add("5,6"));
+	}
+	
+	@Test
+	public void test2(){		
+		assertEquals(6, stringcalc.add("//[*][%]\\n1*2%3"));
+		assertEquals(6, stringcalc.add("//[***]\\n1***2***3"));
+		assertEquals(3, stringcalc.add("//;\\n1;2"));
+		assertEquals(6, stringcalc.add("1\\n2,3"));
 	}
 
 }
